1f7ad8e3fd52821a3defc05b062e3c7b289dacc2
Akash Ponnam <akash.ponnamm@gmail.com>
2025-04-13 00:14:26 -0500

    Update

--- out/static/chunks/src_app_page_tsx_bc31677c._.js
+++ out/static/chunks/src_app_page_tsx_bc31677c._.js
@@ -5,7 +5,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_page_tsx_bc31677c._.js",
   "chunks": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_758b9bc6._.js",
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_c5c84733._.js",
     "static/chunks/node_modules_next_c367bc0b._.js",
     "static/chunks/node_modules_react-icons_fa_index_mjs_d2e2d7f5._.js",
     "static/chunks/node_modules_react-icons_gr_index_mjs_42e3d99f._.js",
